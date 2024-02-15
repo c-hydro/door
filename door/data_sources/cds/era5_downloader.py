cf13cc203373425d90b9d64e6c3a770671f336bd
ltrotter <lucatrotter@gmail.com>
2024-02-15 11:06:16 +0100

    feat(era5): set and check options

--- door/data_sources/cds/era5_downloader.py
+++ door/data_sources/cds/era5_downloader.py
@@ -6,6 +6,10 @@ logger = logging.getLogger(__name__)
 class ERA5Downloader(CDSDownloader):
 
     available_products = ['reanalysis-era5-single-levels']
+    default_options = {
+        'output_format': 'netcdf', # one of netcdf, GeoTIFF
+        'aggregate_daily': 'mean', # one of 'mean', 'max', 'min', 'sum'
+    }
 
     def __init__(self, product) -> None:
         if product not in self.available_products:
@@ -14,3 +18,18 @@ class ERA5Downloader(CDSDownloader):
             raise ValueError(msg)
         
         super().__init__(product)
+
+    def check_options(self, options: dict) -> dict:
+        options = super().check_options(options)
+
+        if 'tif' in options['output_format'].lower():
+            options['output_format'] = 'GeoTIFF'
+        else:
+            logger.warning(f'Output format {options["output_format"]} not supported. Using netcdf')
+            options['output_format'] = 'netcdf'
+        
+        if options['aggregate_daily'].lower() not in ['mean', 'max', 'min', 'sum']:
+            logger.warning(f'Unknown method {options["aggregate_daily"]}. Using mean')
+            options['aggregate_daily'] = 'mean'
+    
+        return options
