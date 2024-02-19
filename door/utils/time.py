c4f6bd49c1e8b75ec9b78ce54e6f67d77550283e
ltrotter <lucatrotter@gmail.com>
2024-02-19 13:52:33 +0100

    feat(time): add option to get dekadal timesteps

--- door/utils/time.py
+++ door/utils/time.py
@@ -3,6 +3,7 @@ import dateutil.relativedelta as dtr
 import numpy as np
 import pandas as pd
 
+from typing import Generator
 import logging
 
 class TimeRange():
@@ -43,22 +44,42 @@ class TimeRange():
                 self.start = end
                 self.end = start
 
-    def get_timesteps_from_tsnumber(self, timesteps_per_year: int) -> list[dt.datetime]:
-        return list(self.gen_timesteps_from_tsnumber(timesteps_per_year))
+    def get_timesteps_from_tsnumber(self, timesteps_per_year: int, get_end = False) -> list[dt.datetime]:
+        return list(self.gen_timesteps_from_tsnumber(timesteps_per_year, get_end = get_end))
     
-    def gen_timesteps_from_tsnumber(self, timesteps_per_year: int) -> dt.datetime:
+    def gen_timesteps_from_tsnumber(self, timesteps_per_year: int, get_end = False) -> Generator[dt.datetime, None, None]:
         """
         This will yield the timesteps to download on a regular frequency by the number of timesteps per year.
         timesteps_per_year is expressed as an integer indicating the number of times per year (e.g. 12 for monthly data, 365 for daily data, etc.).
-        Allows hourly, daily, monthly and yearly data.
+        Allows hourly, daily, dekadly, monthly and yearly data.
         """
-        now = self.start
-        while now <= self.end:
+        if timesteps_per_year == 8760:
+            now = self.start.replace(minute = 0, second = 0)
+        elif timesteps_per_year == 12:
+            now = self.start.replace(day = 1, hour = 0, minute = 0, second = 0)
+        elif timesteps_per_year == 365:
+            now = self.start.replace(hour = 0, minute = 0, second = 0)
+        elif timesteps_per_year == 36:
+            start_day = self.start.day
+            if start_day <= 10:
+                now = self.start.replace(day = 1, hour = 0, minute = 0, second = 0)
+            elif start_day <= 20:
+                now = self.start.replace(day = 11, hour = 0, minute = 0, second = 0)
+            else:
+                now = self.start.replace(day = 21, hour = 0, minute = 0, second = 0)
+        elif timesteps_per_year == 1:
+            now = self.start.replace(month = 1, day = 1, hour = 0, minute = 0, second = 0)
+        else:
+            raise ValueError(f'Invalid data frequency: {timesteps_per_year} times per year is not supported')
+
+        while True:
             yield now
             if timesteps_per_year == 8760:
                 now += dt.timedelta(hours = 1)
             elif timesteps_per_year == 365:
                 now += dt.timedelta(days = 1)
+            elif timesteps_per_year == 36:
+                now = add_dekad(now)
             elif timesteps_per_year == 12:
                 now += dtr.relativedelta(months = 1)
             elif timesteps_per_year == 1:
@@ -66,11 +87,15 @@ class TimeRange():
             else:
                 # dekads are not implemented here, because no data is available at this frequency to download
                 raise ValueError(f'Invalid data frequency: {timesteps_per_year} times per year is not supported')
+            if now > self.end:
+                if get_end:
+                    yield now
+                break
 
     def get_timesteps_from_DOY(self, doy_list: list[int]) -> list[dt.datetime]:
         return list(self.gen_timesteps_from_DOY(doy_list))
     
-    def gen_timesteps_from_DOY(self, doy_list: list[int]) -> dt.datetime:
+    def gen_timesteps_from_DOY(self, doy_list: list[int]) -> Generator[dt.datetime, None, None]:
         """
         This will yield the timesteps to download on a given list of days of the year.
         This is useful for MODIS and VIIRS data that are available at preset DOYs.
@@ -87,7 +112,7 @@ class TimeRange():
     def get_timesteps_from_issue_hour(self, issue_hours: list[int]) -> list[dt.datetime]:
         return list(self.gen_timesteps_from_issue_hour(issue_hours))
     
-    def gen_timesteps_from_issue_hour(self, issue_hours: list) -> dt.datetime:
+    def gen_timesteps_from_issue_hour(self, issue_hours: list) -> Generator[dt.datetime, None, None]:
         """
         This will yield the timesteps to download oa product issued daily at given hours
         """
@@ -113,7 +138,7 @@ def get_time_from_str(string: str, name = None) -> dt.datetime:
     else:
         raise ValueError(f'Invalid format for {name} time: {string}, expected one of' + '; '.join(available_formats))
     
-def get_regular_steps(start: dt.datetime, step_hrs: int, max_steps: int) -> (list[int], list[dt.datetime]):
+def get_regular_steps(start: dt.datetime, step_hrs: int, max_steps: int) -> tuple[list[int], list[dt.datetime]]:
     """
     Compute the forecast steps for the model with regular n-hourly time frequency
     """
@@ -121,6 +146,21 @@ def get_regular_steps(start: dt.datetime, step_hrs: int, max_steps: int) -> (lis
     forecast_steps = np.arange(step_hrs, max_step, step_hrs)
     time_range = [start + dt.timedelta(hours=float(i)) for i in forecast_steps]
     return time_range, forecast_steps
+
+def add_dekad(time: dt.datetime) -> dt.datetime:
+    """
+    Add the dekad to the time
+    """
+    day = time.day
+    if day == 1:
+        return time.replace(day = 11)
+    elif day == 11:
+        return time.replace(day = 21)
+    elif day == 21:
+        next_month = time + dtr.relativedelta(months = 1)
+        return next_month.replace(day = 1)
+    else:
+        raise ValueError('Invalid day for dekad')
 
 # def check_max_steps(self, max_steps_model: int) -> None:
 #     """
